e94b4e808117798a01e164b0136dcd68bbe51287
Clive Lewis <clivethe14@gmail.com>
2020-07-09 16:59:42 +0530

    Change the extension of the file
    the previous form of the code only changed the file format and not the extension name this change resolves that issue

--- image.py
+++ image.py
@@ -5,8 +5,9 @@ path=os.getcwd()+"/image"
 destination=path #path to directory where new converted 'JPEG' file is to be stored
 for filename in os.listdir(path):
   try: #Loop to iterate through source directory file contents
+    nfilename=filename.split(".")
     im=Image.open(path+"/"+filename) #Open image using PIL Library
-    im.save(destination+"/"+"new_"+filename,'JPEG') #Saving converted file to destination
+    im.save(destination+"/"+"new_"+nfilename[0]+".jpeg",'JPEG') #Saving converted file to destination
   except:
     print("Cannot convert {}".format(filename))
     continue
