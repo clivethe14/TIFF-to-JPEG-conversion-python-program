ccf2926f60183106efcc3196c3d136455a3cbbf8
Clive Lewis <clivethe14@gmail.com>
2020-07-11 14:32:06 +0530

    Make code more user friendly
    take input of path in the interpreter rather than specifying in the code

--- image.py
+++ image.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import os
 from PIL import Image
-path=os.getcwd()+"/image"
-destination=path #path to directory where new converted 'JPEG' file is to be stored
-for filename in os.listdir(path):
-  try: #Loop to iterate through source directory file contents
+
+path=input("Enter absolute path to imagess directory:")  #path to source folder conatining .tif images
+destination=input("Enter absolute path to directory the converted files need to be stored in:") #path to directory where new converted 'JPEG' file is to be stored
+
+for filename in os.listdir(path):  #Loop to iterate through source directory file contents
+  try: 
     nfilename=filename.split(".")
     im=Image.open(path+"/"+filename) #Open image using PIL Library
     im.save(destination+"/"+"new_"+nfilename[0]+".jpeg",'JPEG') #Saving converted file to destination
